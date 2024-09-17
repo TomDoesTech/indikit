3b584585a16ed3cf12db58a921beefaa77d25165
Tom Nagle <tomanagle@gmail.com>
2024-09-17 20:45:51 +1000

    update log

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Logo() {
   return (
     <Link href="/">
-      <span className="inline bg-gradient-to-r from-[#e09ef2] to-[#eb03cc] bg-clip-text font-bold text-transparent">
-        LOGO
+      <span className="flex items-center gap-1">
+        <Image src="/favicon.ico" alt="IndiKit" width={20} height={20} />
+        <span className="inline bg-gradient-to-r from-red-500 via-green-500 to-purple-500 bg-clip-text text-xl font-bold text-transparent">
+          IndiKit
+        </span>
       </span>
     </Link>
   );
